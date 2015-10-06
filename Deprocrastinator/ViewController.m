5ebcef51b43399cccbfe0f004a76c67bd49b65fb
Ehsan Jahromi <ehsan.enj@gmail.com>
2015-10-05 18:22:07 -0700

    SwipeRight feature started

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -14,7 +14,7 @@
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @property (strong, nonatomic) NSMutableArray *inputArray;
-
+@property (strong, nonatomic) NSMutableArray *priorityArray;
 @end
 
 @implementation ViewController
@@ -24,6 +24,8 @@
     
     self.inputArray = [[NSMutableArray alloc]init];
     
+    self.priorityArray = [[NSMutableArray alloc]init];
+    //self.priorityArray = @[].mutableCopy;
     
 }
 
@@ -47,21 +49,86 @@
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    [self.priorityArray replaceObjectAtIndex:indexPath.row withObject:[NSNumber numberWithInt:1]];
     cell.textLabel.textColor = [UIColor greenColor];
 }
 
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        [self.inputArray removeObjectAtIndex:indexPath.row];
+        [self.tableView reloadData];
+    }else if(editingStyle == UITableViewCellEditingStyleInsert){
+        
+    }
+    
+}
 
+-(BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath{
+    return YES;
+}
 
 
 - (IBAction)onAddButtonPressed:(UIButton *)sender {
     
-    //cell.textLabel.text = self.inputTextField.text;
     [self.inputArray addObject:self.inputTextField.text];
+    [self.priorityArray addObject:[NSNumber numberWithInt:0]];
     [self.tableView reloadData];
 }
 
 
+- (IBAction)EditButtonPressed:(UIBarButtonItem *)sender {
+    //UIBarButtonItem *editButton = (UIBarButtonItem *)sender;
 
+    if (self.editing) {
+        [sender setTitle:@"Edit"];
+        [self.tableView setEditing:NO animated:YES];
+        self.editing = NO;
+        sender.style = UIBarButtonItemStylePlain;
+        
+    }else{
+        [sender setTitle:@"Done"];
+        self.editing = YES;
+        [self.tableView setEditing:YES animated:YES];
+        sender.style = UIBarButtonItemStyleDone;
+        
+    }
+    
+    [self.tableView reloadData];
+}
+
+- (IBAction)swipeDetected:(UISwipeGestureRecognizer *)sender {
+    CGPoint location = [sender locationInView:self.tableView];
+    NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:location];
+    UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
+    if (UISwipeGestureRecognizerDirectionRight) {
+        int count = [[self.priorityArray objectAtIndex:indexPath.row]intValue];
+        
+        if (count == 0) {
+            count++;
+            
+        }else if(count <= 3){
+            count--;
+        }
+        
+        switch (count) {
+            case 0:
+                cell.textLabel.textColor = [UIColor blackColor];
+                break;
+            case 1:
+                cell.textLabel.textColor = [UIColor greenColor];
+            case 2:
+                cell.textLabel.textColor = [UIColor yellowColor];
+            case 3:
+                cell.textLabel.textColor = [UIColor redColor];
+            default:
+                break;
+        }
+        
+        
+        [self.priorityArray replaceObjectAtIndex:indexPath.row withObject:[NSNumber numberWithInt:count]];
+    }
+}
 
 
 
